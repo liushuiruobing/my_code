7963954872ae8d00b2610860260cb10dfee7e8b8
liushuiruobing <wenlianpeng521@163.com>
2018-10-24 09:44:20 +0800

    添加海康摄像机的控制
    WPF 加载WindowsForm的PictureBox来显示海康摄像机的图片

--- WPF/DR30A_CS(WPF)/DR30A_CS(WPF)/MainWindow.xaml.cs
+++ WPF/DR30A_CS(WPF)/DR30A_CS(WPF)/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Runtime.InteropServices;
+using MvCamCtrl.NET;
+using DeviceSource;
+
 
 namespace DR30A_CS_WPF_
 {
@@ -44,15 +39,22 @@ namespace DR30A_CS_WPF_
     /// </summary>
     public partial class MainWindow : Window
     {
-
         CustomColor customColor;
+
+        //摄像头相关
+        CameraOperator m_pOperator;
+        ComboBox m_CameraDeviceList;
+        MyCamera.MV_CC_DEVICE_INFO_LIST m_pDeviceList;
+        System.Windows.Forms.PictureBox m_CameraPicBox = null;
+
         public MainWindow()
         {
             InitializeComponent();
             InitWindowSize();
             InitControlColor();
+            InitCamera();
 
-            SystemParameters.StaticPropertyChanged += SystemParameters_StaticPropertyChanged;
+            SystemParameters.StaticPropertyChanged += SystemParameters_StaticPropertyChanged;  //监控任务栏变化
         }
 
         //设置窗体的大小
@@ -82,17 +84,26 @@ namespace DR30A_CS_WPF_
             listView_TestAction.Background = customColor.BackGroundColor;
         }
 
-        private void image_Close_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void InitCamera()
+        {
+            m_CameraPicBox = pictureHost.Child as System.Windows.Forms.PictureBox;
+            m_CameraDeviceList = new ComboBox();
+            m_pDeviceList = new MyCamera.MV_CC_DEVICE_INFO_LIST();
+            m_pOperator = new CameraOperator();
+
+            CarmerDeviceListAcq();
+        }
+
+        private void image_Close_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             this.Close();
         }
-      
+
         //鼠标左键按下后可移动窗体
         private void DragWindow(object sender, MouseButtonEventArgs e)
         {
             //this.DragMove();
         }
-
 
         private void SystemParameters_StaticPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
@@ -101,6 +112,242 @@ namespace DR30A_CS_WPF_
             {
                 this.MinHeight = this.MaxHeight = SystemParameters.WorkArea.Height;
             }
+        }
+
+        private void button_CheckCarmer_Click(object sender, RoutedEventArgs e)
+        {
+            CarmerDeviceListAcq();
+        }
+
+        //查找摄像头
+        private void CarmerDeviceListAcq()
+        {
+            //System.GC.Collect();
+            m_CameraDeviceList.Items.Clear();
+            int nRet = CameraOperator.EnumDevices(MyCamera.MV_GIGE_DEVICE | MyCamera.MV_USB_DEVICE, ref m_pDeviceList);
+            if (0 != nRet)
+            {
+                MessageBox.Show("枚举设备失败!");
+                return;
+            }
+
+            //在窗体列表中显示设备名
+            for (int i = 0; i < m_pDeviceList.nDeviceNum; i++)
+            {
+                MyCamera.MV_CC_DEVICE_INFO device = (MyCamera.MV_CC_DEVICE_INFO)Marshal.PtrToStructure(m_pDeviceList.pDeviceInfo[i], typeof(MyCamera.MV_CC_DEVICE_INFO));
+                if (device.nTLayerType == MyCamera.MV_GIGE_DEVICE)
+                {
+                    IntPtr buffer = Marshal.UnsafeAddrOfPinnedArrayElement(device.SpecialInfo.stGigEInfo, 0);
+                    MyCamera.MV_GIGE_DEVICE_INFO gigeInfo = (MyCamera.MV_GIGE_DEVICE_INFO)Marshal.PtrToStructure(buffer, typeof(MyCamera.MV_GIGE_DEVICE_INFO));
+                    if (gigeInfo.chUserDefinedName != "")
+                    {
+                        m_CameraDeviceList.Items.Add("GigE: " + gigeInfo.chUserDefinedName + " (" + gigeInfo.chSerialNumber + ")");                        
+                    }
+                    else
+                    {
+                        m_CameraDeviceList.Items.Add("GigE: " + gigeInfo.chManufacturerName + " " + gigeInfo.chModelName + " (" + gigeInfo.chSerialNumber + ")");                      
+                    }
+                }
+                else if (device.nTLayerType == MyCamera.MV_USB_DEVICE)
+                {
+                    IntPtr buffer = Marshal.UnsafeAddrOfPinnedArrayElement(device.SpecialInfo.stUsb3VInfo, 0);
+                    MyCamera.MV_USB3_DEVICE_INFO usbInfo = (MyCamera.MV_USB3_DEVICE_INFO)Marshal.PtrToStructure(buffer, typeof(MyCamera.MV_USB3_DEVICE_INFO));
+                    if (usbInfo.chUserDefinedName != "")
+                    {
+                        m_CameraDeviceList.Items.Add("USB: " + usbInfo.chUserDefinedName + " (" + usbInfo.chSerialNumber + ")");
+                    }
+                    else
+                    {
+                        m_CameraDeviceList.Items.Add("USB: " + usbInfo.chManufacturerName + " " + usbInfo.chModelName + " (" + usbInfo.chSerialNumber + ")");                      
+                    }
+                }
+            }
+
+            //选择第一项
+            if (m_pDeviceList.nDeviceNum != 0)
+            {
+                m_CameraDeviceList.SelectedIndex = 0;
+            }
+        }
+
+        //打开设备
+        private void button_OpenCarmer_Click(object sender, RoutedEventArgs e)
+        {          
+            if (m_pDeviceList.nDeviceNum == 0 || m_CameraDeviceList.SelectedIndex == -1)
+            {
+                MessageBox.Show("无设备，请选择");
+                return;
+            }
+
+            //获取选择的设备信息
+            MyCamera.MV_CC_DEVICE_INFO device = (MyCamera.MV_CC_DEVICE_INFO)Marshal.PtrToStructure(m_pDeviceList.pDeviceInfo[m_CameraDeviceList.SelectedIndex], typeof(MyCamera.MV_CC_DEVICE_INFO));          
+            int nRet = m_pOperator.Open(ref device);   //打开设备
+            if (MyCamera.MV_OK != nRet)
+            {
+                MessageBox.Show("设备打开失败!");
+                return;
+            }
+
+            //设置采集连续模式
+            m_pOperator.SetEnumValue("AcquisitionMode", 2);// 工作在连续模式
+            m_pOperator.SetEnumValue("TriggerMode", 0);    // 连续模式
+            CarmeraStartGrab();
+
+            SetCtrlWhenOpen();            
+        }
+
+
+        //关闭摄像头设备
+        private void button_CloseCarmer_Click(object sender, RoutedEventArgs e)
+        {
+            m_pOperator.Close();
+            SetCtrlWhenClose();
+        }
+
+        //开启摄像头时，设置控件状态
+        private void SetCtrlWhenOpen( )
+        {
+            button_OpenCarmer.IsEnabled = false;
+            button_CloseCarmer.IsEnabled = true;
+            button_SetParam.IsEnabled = true;
+            button_GetParam.IsEnabled = true;
+        }
+
+        //关闭摄像头时，设置控件状态
+        private void SetCtrlWhenClose()
+        {
+            button_OpenCarmer.IsEnabled = true;
+            button_CloseCarmer.IsEnabled = false;
+            button_SetParam.IsEnabled = false;
+            button_GetParam.IsEnabled = false;
+        }
+
+        //开始采集
+        private void CarmeraStartGrab()
+        {
+            int nRet = m_pOperator.StartGrabbing();
+            if (MyCamera.MV_OK != nRet)
+            {
+                MessageBox.Show("开始取流失败！");
+                return;
+            }
+
+            //显示
+            nRet = m_pOperator.Display(m_CameraPicBox.Handle);
+            if (MyCamera.MV_OK != nRet)
+            {
+                MessageBox.Show("显示失败！");
+            }
+        }
+
+        private void button_SetParam_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_GetParam_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestExampleImport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestExampleExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestExampleUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestExampleDown_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestExampleDeleteSelected_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestExampleDeleteAll_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_AddExample_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestActionRebortHome_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestActionImportImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestActionUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestActionDown_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestActionDeleteSelected_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestActionDeleteAll_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestActionSetDelay_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestActionCollectPicture_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_TestActionArea_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Run_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Suspend_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Stop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            m_pOperator.Close();
         }
     }
 }
